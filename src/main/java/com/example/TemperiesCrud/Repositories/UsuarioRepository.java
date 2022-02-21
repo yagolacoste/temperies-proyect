eb83d52efc4a7b8ef09cf3dfb92cb8d101fd997c
yago <yagolacos@gmail.com>
2022-02-21 12:26:19 -0300

    anda bien

--- src/main/java/com/example/TemperiesCrud/Repositories/UsuarioRepository.java
+++ src/main/java/com/example/TemperiesCrud/Repositories/UsuarioRepository.java
@@ -1,12 +1,14 @@
 package com.example.TemperiesCrud.Repositories;
 
-import com.example.TemperiesCrud.Models.*;
+import java.util.ArrayList;
+
+import com.example.TemperiesCrud.Models.UsuarioModel;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
 
-
-public interface UsuarioRepository extends CrudRepository<PersonaModel,Long>{
-    
-
-    
 }
--- src/main/java/com/example/TemperiesCrud/TemperiesCrudApplication.java
+++ src/main/java/com/example/TemperiesCrud/TemperiesCrudApplication.java
@@ -3,7 +3,7 @@ package com.example.TemperiesCrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages="Controllers")
+@SpringBootApplication
 public class TemperiesCrudApplication {
 
 	public static void main(String[] args) {
